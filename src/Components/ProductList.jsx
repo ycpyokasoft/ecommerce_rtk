1bfd42cc9a38f0c5855a179c422a32b8c123eb69
ycpyokasoft <ryoka@ycp.edu>
2025-01-13 21:32:34 -0500

    Final project

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addItemToCart } from './CartSlice';
 
 const ProductList = () => {
 
@@ -9,11 +12,28 @@ const ProductList = () => {
     { id: 3, name: 'Product C', price: 30 },
   ];
 
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]);
+
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+    setDisabledProducts([...disabledProducts, product.id]);
+  };
+  
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-     
+        {products.map(product => (
+          <li key={product.id} className="product-list-item">
+          <span>{product.name} - ${product.price}</span>
+          <button
+            className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled':''}`}
+            onClick={() => handleAddToCart(product)}
+            disabled={disabledProducts.includes(product.id)}
+            >Add to Cart</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
